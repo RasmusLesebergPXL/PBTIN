1e20058339b3d1790956305d660cf0c8dc57f464
student <rasmus.leseberg@student.pxl.be>
2023-04-23 23:03:36 +0200

    Reorganized Automation folder
    - Redid all exercises until H5

--- Jaar_2/Automation/4.variables/ex1_varfiles/.vagrant/plugins/gems/2.7.4/gems/vagrant-hosts-2.9.0/lib/vagrant-hosts.rb
+++
@@ -1,6 +0,0 @@
-module VagrantHosts
-  require 'vagrant-hosts/version'
-  require 'vagrant-hosts/plugin'
-end
-
-I18n.load_path << File.expand_path('../templates/locales/en.yml', File.dirname(__FILE__))
--- Jaar_2/Automation/5.vault/clean_known_hosts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ssh-keygen -R [127.0.0.1]:2222
-ssh-keygen -R [127.0.0.1]:2200
