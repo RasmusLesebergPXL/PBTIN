b7f8da00aa26e8aeef51cde5cfc0f65a711f4785
student <rasmus.leseberg@student.pxl.be>
2023-06-09 09:59:37 +0200

    CloudServices updates

--- Jaar_2/CloudServices/PE/CloudToDoApp/frontend/src/app/CarrouselItem.model.ts
+++
@@ -1,3 +0,0 @@
-export interface CarrouselItem {
-    url: string;
-}
---
+++ Jaar_2/CloudServices/pe/CloudToDoApp/frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiurl: 'https://otyfthehya.execute-api.us-east-1.amazonaws.com/initial_deployment',
+};
