5357b9a1ed495953f58ec3de1a28bb1973590bc6
student <rasmus.leseberg@student.pxl.be>
2023-06-15 10:34:49 +0200

    added slides and changed file names

--- Jaar_2/SecurityAdvanced/repos/yellow-team-oauth-RasmusLesebergPXL/api/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-openssl x509 -inform DER -in /root/.aspnet/https/root.cer -out /root/.aspnet/https/root.crt
-cp /root/.aspnet/https/root.crt /usr/local/share/ca-certificates/
-update-ca-certificates
-
-dotnet api.dll
