22f44999da9487df747e226713147ae04d186c8a
student <rasmus.leseberg@student.pxl.be>
2023-06-22 13:54:46 +0200

    cleanup ItEssentials folder

--- Jaar_1/IT_Essentials/IT-essentials-oefeningen/5_functies/test - Copy.py
+++
@@ -1,8 +0,0 @@
-from random import randrange, randint
-
-text = input("enter a string to convert into ascii values:")
-ascii_values = []
-for character in text:
-    ascii_values.append(ord(character) + randrange(2,24,2))
-
-print(ascii_values)
--- Jaar_1/IT_Essentials/IT-essentials-oefeningen/5_functies/test2 - Copy.py
+++
@@ -1,6 +0,0 @@
-
-for i in range(3,13):
-    print("{:d} {:14d} {:5d}".format(i, i*i, i*i*i))
-
-def toon_bord(spelbord):    print("  1 2 3")    for rij in range(3):        print(rij + 1, end=" ")        for kol in range(3):            print(spelbord[rij][kol], end=" ")        print()def main():    bord = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]    bord[1][1] = "X"    bord[0][2] = "O"    toon_bord(bord)
-
