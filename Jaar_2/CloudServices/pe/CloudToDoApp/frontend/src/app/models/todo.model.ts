0682fe2f42944479c21ce6540973fd61d918402d
student <rasmus.leseberg@student.pxl.be>
2023-06-30 21:15:39 +0200

    DevOps dir restructure and added readme

--- Jaar_2/CloudServices/pe/CloudToDoApp/frontend/src/app/models/todo.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Todo {
-  title: string;
-  label: string;
-  completed: boolean;
-  createdAt: Date;
-  _id?:string;
-}
--- Jaar_2/CloudServices/pe/CloudToDoApp/frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiurl: 'https://otyfthehya.execute-api.us-east-1.amazonaws.com/initial_deployment',
-};
